fd2db9d6d8a566fe359d54a19e1676ebea795dfe
siman-man <k128585@ie.u-ryukyu.ac.jp>
2016-12-12 23:05:26 +0900

    133byte

--- solve.rb
+++ solve.rb
@@ -1,12 +1,1 @@
-GC.disable;$f=$<.map &:chomp
-def d(y,x)
-$f[y][x]!=' '&&return
-$f[y][x+1]==?G&&($f[y][x]=?:;puts$f;exit)
-$f[y][x]=?:
-d(y+1,x)
-d(y,x-1)
-d(y-1,x)
-d(y,x+1)
-$f[y][x]=' '
-end
-$..times{|y|x=0;$f[y].chars{|c|c==?S&&d(y,x+1);x+=1}}
+d=->y,x{f=$f[y];f[x]==?G&&(puts$f;exit);f[x]>?!||(f[x]=?:;d[y+1,x];d[y,x-1];d[y-1,x];d[y,x+1];f[x]=' ')};d[($f=*$<).index{|s|s>?R},1]
